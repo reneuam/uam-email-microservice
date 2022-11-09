7fb3a3eb885b2a85fb296fd09c952dd21b5a7dca
reneuam <renet@uassistme.com>
2022-11-09 14:12:12 -0600

    Set email microservice

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { RabbitMQ } from './common/constants';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.RMQ,
+    options: {
+      url: [process.env.AMQP_URL],
+      queue: RabbitMQ.EmailQueue
+    },
+  });
+  await app.listen();
 }
 bootstrap();
