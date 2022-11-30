9ae37883caf01be4a58d1e5e2961327c475f1b3c
reneuam <renet@uassistme.com>
2022-11-30 01:17:53 -0600

    Set email confuguration based on .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,30 +1,35 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MailerModule } from '@nestjs-modules/mailer';
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
+import { UnpluggedModule } from './api/unplugged/unplugged.module';
+import * as path from 'path';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MailerModule.forRoot({
       transport: {
-        host: 'mail.uassistme.com',
-        port: 465,
+        host: process.env.MAIL_HOST,
+        port: process.env.MAIL_PORT,
         secure: true,
         auth: {
-          user: "unplugged@uassistme.com",
-          pass: "2fjC-!@bMm[Q3$(hIg)M$p02",
+          user: process.env.MAIL_USER,
+          pass: process.env.MAIL_PASSWORD,
         },
       },
       defaults: {
-        from: '"Unplugged" <unplugged@uassistme.com>',
+        from: `"Unplugged" <${process.env.MAIL_USER}>`,
       },
       template: {
-        dir: __dirname + '/templates',
+        dir: path.join(__dirname, '..', 'templates'),
         adapter: new HandlebarsAdapter(),
         options: {
           strict: true,
         },
       },
     }),
+    UnpluggedModule,
   ]
 })
 export class AppModule {}
