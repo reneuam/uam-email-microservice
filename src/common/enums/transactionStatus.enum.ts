d222172bed1e023cb7c8660c17fda05072769b18
reneuam <renet@uassistme.com>
2023-01-18 09:55:00 -0600

    Add transaction status enum

---
+++ src/common/enums/transactionStatus.enum.ts
@@ -0,0 +1,12 @@
+export enum TransactionStatus {
+  createdByBP = 1,
+  createdByTL = 2,
+  createdByAdmin = 3,
+  createdByHR = 4,
+  approvedByTL = 5,
+  approvedByHR = 6,
+  deniedByTL = 7,
+  deniedByHR = 8,
+  cancelledByBP = 9,
+  cancelledByHR = 10
+}
